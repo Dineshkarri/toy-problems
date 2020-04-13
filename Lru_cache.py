ef5fee836a0e966c77f224084794102f44696c37
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-04-13 22:22:48 +0530

    added Lru_test class and test methods

--- Lru_cache.py
+++ Lru_cache.py
@@ -1,7 +1,7 @@
 class Lru_cache:
    
     def __init__(self):
-        self.lists = []
+        self.list1 = []
 
     def put(self,x):
         pass
@@ -11,3 +11,23 @@ class Lru_cache:
     
     def get_cache(self):
         pass
+
+cache = Lru_cache()
+
+
+class Lru_test:
+
+    def testput(self):
+        val = cache.put(2)
+        self.assertEqual(val,2)
+
+    def testget(self):
+        val1 = cache.get()
+        self.assertEqual(val1,2)
+
+    def testget_cache(self):
+        var = cache.get_cache()
+        self.assertEqual(var,[2])
+
+if __name__==   "__main__":
+  Lru_test()
