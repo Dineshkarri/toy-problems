70fb56c5f845074908b991b438a46aaff07bc3ab
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-04-13 19:28:41 +0530

    added methods

--- Lru_cache.py
+++ Lru_cache.py
@@ -3,3 +3,11 @@ class Lru_cache:
     def __init__(self):
         self.lists = []
 
+    def put(self,x):
+        pass
+
+    def get(self,y):
+        pass
+    
+    def get_cache(self):
+        pass
